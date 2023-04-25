9f13bf9294f8d8bd09c3c1720fd516f1286da4fe
AusafMohi <123327929+AusafMohi@users.noreply.github.com>
2023-04-25 15:51:50 -0500

    Add files via upload

---
+++ am_Assignment_4B.rb
@@ -0,0 +1,61 @@
+#------------------------------------------------------------------------------- 
+# Assignment:    Assignment 4B 
+# 
+# Program Name:  am_Assignment_4B.rb  
+#                 
+# 
+# Purpose:       The purpose of this program is  
+#                to print a recipe. 
+# 
+# Author:        Ausaf M.
+# Course:        231CIS109.950 
+# 
+# Created:       09/17/2022 
+# 
+#------------------------------------------------------------------------------ 
+ 
+
+degree       =  "\u00b0".force_encoding('utf-8') 
+oneHalf      =  "\u00bd".force_encoding('utf-8') 
+oneQuarter   =  "\u00bc".force_encoding('utf-8') 
+threeQuarter =  "\u00be".force_encoding('utf-8') 
+oneThird     =  "\u2153".force_encoding('utf-8') 
+
+print ("Apple Bavarian Torte\n\n")
+print ("A classic European torte baked in a springform pan.\n")
+print ("Cream cheese, sliced almonds, and apples make this the \nbest holiday treat (12 servings).\n\n")
+
+print ("INGREDIENTS\n")
+print (oneHalf)
+print (" cup butter\n")
+print (oneHalf)
+print (" cup white sugar\n")
+print (oneQuarter)
+print (" teaspoon vanilla extract\n")
+print ("1 cup all purpose flour\n")
+print ("1 (8 ounce) package cream cheese\n")
+print (oneQuarter)
+print (" cup white sugar\n")
+print ("1 egg \n")
+print (oneQuarter)
+print (" teaspoon vanilla extract\n")
+print ("6 apples - peeled, cored\n")
+print (oneThird)
+print (" cup white sugar\n")
+print (oneHalf)
+print (" teaspoon ground cinnamon\n")
+print (oneQuarter)
+print (" cup sliced almonds\n\n")
+
+print ("DIRECTIONS\n")
+print ("1. Preheat oven to 450'F (230'C).\n")
+print ("2. Cream together butter, sugar, vanilla, and flour.\n")
+print ("3. Press crust mixture into the flat bottom of a 9-inch\n   springform pan Set aside.\n")
+print ("4. In a medium bowl, blend cream cheese and sugar. Beat in
+   egg and vanilla. Pour cheese mixture over crust.\n")
+print ("5. Toss apples with sugar and cinnamon. Spread apple mixture
+   over all.\n")
+print ("6. Bake for 10 minutes. Reduce heat to 400F (200`c) and
+   continue baking for 25 minutes.\n")
+print ("7. Sprinkle almonds over top of torte. Continue baking until
+   lightly browned. Cool before removing from pan.\n")
